76aaae7b1a16b63224d81f1df9b08c15cdb2a4ae
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-09-26 11:14:46 -0700

    employee class finished

---
+++ employee.cpp
@@ -0,0 +1,25 @@
+#include "employee.h"
+using namespace std;
+
+Employee::Employee(const char* Name, const char* Address, const char* Job, double Hours, int Dob) {
+	name = Name;
+	address = Address;
+	job = Job;
+	hours = Hours;
+	dob = Dob;
+}
+
+Employee::~Employee() {
+	name = null;
+	address = null;
+	job = null;
+	hours = 0;
+	dob = 0;
+}
+
+void Employee::display() {
+	cout << name << endl;
+	cout << address << endl;
+	cout << hours << endl;
+	cout << dob << endl;
+}
