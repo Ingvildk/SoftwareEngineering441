9fe8633c4791eb165a060e2815368467d50a6e75
Logan Garcia <logangarcia@fastmail.fm>
2016-09-27 19:22:07 -0700

    modified employee files

--- employee.cpp
+++ employee.cpp
@@ -1,20 +1,35 @@
 #include "employee.h"
+#include <cstring>
+
 using namespace std;
 
 Employee::Employee(const char* Name, const char* Address, const char* Job, double Hours, int Dob) {
-	name = Name;
-	address = Address;
-	job = Job;
+	name = new char[strlen(Name) + 1];
+	address = new char[strlen(Address) + 1];
+	job = new char[strlen(Job) + 1];
 	hours = Hours;
 	dob = Dob;
+	
+	for (int i = 0; Name[i] != '\0'; i++)
+        name[i] = Name[i];
+
+    name[strlen(Name)] = '\0';
+    
+    for (int i = 0; Address[i] != '\0'; i++)
+        address[i] = Address[i];
+
+    address[strlen(Address)] = '\0';
+    
+    for (int i = 0; Job[i] != '\0'; i++)
+        job[i] = Job[i];
+
+    job[strlen(Job)] = '\0';
 }
 
 Employee::~Employee() {
-	name = null;
-	address = null;
-	job = null;
-	hours = 0;
-	dob = 0;
+	delete [] name;
+	delete [] address;
+	delete [] job;
 }
 
 void Employee::display() {
