d8866c68c3e46d5c05789809ddcbbdc461dd441b
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-10-04 18:40:30 -0700

    Working on updating the cpp file

--- company.cpp
+++ company.cpp
@@ -1,8 +1,41 @@
 #include "company.h"
+#include "store.h"
+#include <string>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-Company::Company(const char*, const char*) {
-	
-} 
+Company::Company(string Name, string Address, int Id) {
+	name = Name;
+	address = Address;
+	ID = Id;
+}
+
+void Company::addStore(const char Name, const char Address, int Id) {
+		name = Name;
+		address = Address;
+		ID = Id;
+	}
+
+void Comapany::addStore(string Name, string Address, int Id) {
+	name = Name;
+	address = Address;
+	id = Id;
+}
+void Company::removeStore(string storeName) {
+	if ( std::find(vector.begin(), vector.end(), StoreName) != vector.end() )
+	   // DELETE THIS STORE
+	else
+	   cout << "The Store does not exists for this company" << endl;
+}
+void Company::changeStore(string, string, int);
+void Company::displayStore(string);
+void Company::displayStores();
+void Company::addEmployee(string, string, string, double, int);
+void Company::removeEmployee(string, string);
+void Company::changeEmployee(string, string, double);
+void Company::displayEmployee(string, string);
+void Company::displayEmployees();
+void Company::setName(string);
+void Company::setAddress(cstring);	
--- company.h
+++ company.h
@@ -1,33 +1,34 @@
 #ifndef _COMPANY_H
 #define _COMPANY_H
-
+#include <string>
 #include "employee.h"
 #include "store.h"
 #include <vector>
 
 class Company {
     private:
-        std::vector<Employee*> employees;   //list of employees
-        std::vector<Store*> stores;         //list of stores
-        char *name;                         //name of company
-        char *address;                      //current address of company
+        std::vector<Employee> employees;   //list of employees
+        std::vector<Store> stores;         //list of stores
+        string name;                         //name of company
+        string address;                      //current address of company
+        int ID;
     public:
-        Company(const char*, const char*);
+        Company(string Name, string Address, int Id);
         ~Company();
-        char *getName() { return name; }
-        char *getAddress() { return address; }
-        void addStore(const char*, const char*, int);
-        void removeStore(const char*);
-        void changeStore(const char*, const char*, int);
-        void displayStore(const char*);
+        string getName() { return name; }
+        string getAddress() { return address; }
+        void addStore(string, string, int);
+        void removeStore(string);
+        void changeStore(string, string, int);
+        void displayStore(string);
         void displayStores();
-        void addEmployee(const char*, const char*, const char*, double, int);
-        void removeEmployee(const char*, char);
-        void changeEmployee(const char*, const char*, double);
-        void displayEmployee(const char*, char);
+        void addEmployee(string, string, string, double, int);
+        void removeEmployee(string, string);
+        void changeEmployee(string, string, double);
+        void displayEmployee(string, string);
         void displayEmployees();
-        void setName(const char*);
-        void setAddress(const char*);
+        void setName(string);
+        void setAddress(cstring);
 };
 
 #endif
