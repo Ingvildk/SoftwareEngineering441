84ad3da09cc040aa24cf0a8ab695ec64ecc4b3a8
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-10-07 13:05:41 -0700

    Fixed char from string

--- store.h
+++ store.h
@@ -1,32 +1,32 @@
 #ifndef _STORE_H
 #define _STORE_H
-
 #include "employee.h"
 #include "inventory.h"
+#include <string>
 #include <vector>
 
 class Store {
     private:
-        std::vector<Employee*> employees;   //list of employees
-        Inventory *inventory;               //inventory for the store
-        char *name;                         //name of store
-        char *address;                      //current address of store
+        std::vector<Employee> employees;   //list of employees
+        Inventory inventory;               //inventory for the store
+        string name;                         //name of store
+        string address;                      //current address of store
         int id;                             //id of store
-        void searchEmployee (const char*, char);
+        void searchEmployee (string, string);
     public:
-        Store(const char*, const char*, int);
+        Store(string, string, int);
         ~Store();
-        Inventory *getInventory() { return inventory; }
-        char *getName() { return name; }
-        char *getAddress() { return address; }
+        Inventory getInventory() { return inventory; }
+        string getName() { return name; }
+        string getAddress() { return address; }
         int getId() { return id; }
-        void addEmployee(const char*, const char*, const char*, double, int);
-        void removeEmployee(const char*, char);
-        void changeEmployee(const char*, const char*, double);
-        void displayEmployee(const char*, char);
+        void addEmployee(string, string, string, double, int);
+        void removeEmployee(string, string);
+        void changeEmployee(string, string, double);
+        void displayEmployee(string, string);
         void displayEmployees();
-        void setName(const char*);
-        void setAddress(const char*);
+        void setName(string);
+        void setAddress(string);
         void setId(int i) { id = i; }
 };
 
