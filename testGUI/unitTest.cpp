a89fe04261fbb5dc5254e027c3d64de0854726de
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-11-14 08:55:45 -0800

    Started making testing

---
+++ testGUI/unitTest.cpp
@@ -0,0 +1,9 @@
+#include "company.h"
+#include "inventory.h"
+#include "transaction.h"
+#include <iostream>
+using namespace std;
+int main () {
+	cout << "--------This is the testing file--------";
+	return 0;
+}
