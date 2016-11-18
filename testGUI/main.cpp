7bd4473170c79a8dc4891422d2574425a4cafdec
Kayla-Leigh <libuszowskik@gmail.com>
2016-11-18 08:46:45 -0800

    Login
    Login added, address needs to be changed to read the test file for
    loginwindow.cpp, for now not working unless you do.

--- testGUI/main.cpp
+++ testGUI/main.cpp
@@ -2,24 +2,27 @@
 #include <QApplication>
 #include "form_company.h"
 #include <fstream>
+#include "loginwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    LoginWindow w;
     w.show();
-    std::ifstream myfile;
-    myfile.open("company.txt");
-    //possible listener for this if statement. Currently does not work to serve as file detection -Dennis
-    if(myfile.is_open()){
-        myfile.close();
-    }
-    else{
-        form_company fc;
-        Qt::WindowFlags flags = fc.windowFlags();
-        fc.setWindowFlags(flags | Qt::WindowStaysOnTopHint);
-        fc.setWindowModality(Qt::ApplicationModal);
-        fc.show();
-    }
+//    MainWindow w;
+//    w.show();
+//    std::ifstream myfile;
+//    myfile.open("company.txt");
+//    //possible listener for this if statement. Currently does not work to serve as file detection -Dennis
+//    if(myfile.is_open()){
+//        myfile.close();
+//    }
+//    else{
+//        form_company fc;
+//        Qt::WindowFlags flags = fc.windowFlags();
+//        fc.setWindowFlags(flags | Qt::WindowStaysOnTopHint);
+//        fc.setWindowModality(Qt::ApplicationModal);
+//        fc.show();
+//    }
     return a.exec();
 }
