bb23f50f59170b7833a95e02a674c46d6a31eb5c
dennismesina <dennismesina@gmail.com>
2016-11-20 12:14:13 -0800

    kjgjhvjhvjhvjgvj
    jkbkjbkbkbkj -Dennis

--- testGUI/main.cpp
+++ testGUI/main.cpp
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
     std::ifstream myfile;
     myfile.open("company.txt");
     //possible listener for this if statement. Currently does not work to serve as file detection -Dennis
-    if(myfile.is_open()){
+    //if(myfile.is_open()){
         myfile.close();
-    }
-    else{
+    //}
+    //else{
         form_company fc;
         Qt::WindowFlags flags = fc.windowFlags();
         fc.setWindowFlags(flags | Qt::WindowStaysOnTopHint);
         fc.setWindowModality(Qt::ApplicationModal);
         fc.show();
-    }
+    //}
     return a.exec();
 }
