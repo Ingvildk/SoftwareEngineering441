57d720cea5167d2473ac36366c48700dfb369652
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-11-21 18:07:18 -0800

    Working

--- testGUI/testwindow3.cpp
+++ testGUI/testwindow3.cpp
@@ -23,8 +23,8 @@ void testwindow3::on_fireButton_clicked()
     //Company.removeEmployee(temp);
     //qDebug() << temp;
 
-    //QString temp = ui->IDEdit->toPlainText();
-    //temp.toInt();
+    QString temp = ui->IDEdit->toPlainText();
+    temp.toInt();
     //singleton company will call the removeEmployee
 
     //removeEmployee(temp);
@@ -34,15 +34,15 @@ void testwindow3::on_fireButton_clicked()
 
 void testwindow3::on_hireButton_clicked()
 {
-//    QString id = ui->IDEdit->toPlainText();
-//    id.toDouble();
-//    QString name = ui->nameEdit->toPlainText();
-//    QString address = ui->addressEdit->toPlainText();
-//    QString job = ui->jobEdit->toPlainText();
-//    QString sal = ui->salEdit->toPlainText();
-//    sal.toDouble();
-//    QString dob = ui->DOBEdit->toPlainText();
-//    dob.toDouble();
+      QString id = ui->IDEdit->toPlainText();
+      id.toDouble();
+      QString name = ui->nameEdit->toPlainText();
+      QString address = ui->addressEdit->toPlainText();
+      QString job = ui->jobEdit->toPlainText();
+      QString sal = ui->salEdit->toPlainText();
+      sal.toDouble();
+      QString dob = ui->DOBEdit->toPlainText();
+      dob.toDouble();
     //singleton company will call the removeEmployee
     // addEmployee(int, string, string, string, double, int)
     //addEmployee(id, name, address, job, sal, dob);
