a5864caf73160a9c084749bb46376683477b62ac
dennismesina <dennismesina@gmail.com>
2016-12-01 22:05:00 -0800

    Pseudo Setup for company
    If there is no info in company file, it will prompt. -Dennis

--- testGUI/main.cpp
+++ testGUI/main.cpp
@@ -9,20 +9,23 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     LoginWindow w;
     w.show();
-
+    form_company fc;
+    Qt::WindowFlags flags = fc.windowFlags();
+    fc.setWindowFlags(flags | Qt::WindowStaysOnTopHint);
+    fc.setWindowModality(Qt::ApplicationModal);
     std::ifstream myfile;
+    std::string check = "";
     myfile.open("company.txt");
     //possible listener for this if statement. Currently does not work to serve as file detection -Dennis
-    //if(myfile.is_open()){
-        myfile.close();
-    //}
-    //else{
-        form_company fc;
-        Qt::WindowFlags flags = fc.windowFlags();
-        fc.setWindowFlags(flags | Qt::WindowStaysOnTopHint);
-        fc.setWindowModality(Qt::ApplicationModal);
+    getline(myfile, check);
+    myfile.close();
+    if(check == ""){
+        remove("company.txt");
         fc.show();
-    //}
+    }
+    else{
+        fc.close();
+    }
 
 //    MainWindow w;
 //    w.show();
